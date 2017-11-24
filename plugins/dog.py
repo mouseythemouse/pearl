51deb272bca664b42f4b86bb5f7c80eb80448c07
Shwetha K <skunnamalt@gmail.com>
2017-11-24 09:06:28 -0500

    Update dog.py

--- plugins/dog.py
+++ plugins/dog.py
@@ -1,32 +1,18 @@
-
 import asyncio
-
 import hangups
 
-class Dog:
+from command import Command
 
-	def __init__(self, client):
-		self.client = client
+class Dog(Command):
 
-	@asyncio.coroutine
-	def handle(self, pearl, args, event):
+	def __init__(self, pearl):
+		self.pearl = pearl
+		self.client = pearl.client
+
+	def handle(self, args, event):
 		dog = self.client.upload_image("https://upload.wikimedia.org/wikipedia/commons/thumb/5/58/Shiba_inu_taiki.jpg/220px-Shiba_inu_taiki.jpg")
 		
-		request = hangups.hangouts_pb2.SendChatMessageRequest(
-			request_header=self.client.get_request_header(),
-			event_request_header=hangups.hangouts_pb2.EventRequestHeader(
-				conversation_id=hangups.hangouts_pb2.ConversationId(
-					id=event.conversation_id.id
-				),
-				client_generated_id=self.client.get_client_generated_id(),
-			),
-			message_content=hangups.hangouts_pb2.MessageContent(
-				segment=[
-					hangups.UploadedImage(dog).serialize()
-				],
-			),
-		)
-		yield from self.client.send_chat_message(request)
+		asyncio.run_coroutine_threadsafe(self.send(dog, event.conversation_id.id), self.pearl.loop)
 
-def initialize(client):
-	return Dog(client)
+def initialize(pearl):
+	return Dog(pearl)
