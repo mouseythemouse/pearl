736db172fb9ccddb6290ed9d8096aa1bf11fec8a
Shwetha K <skunnamalt@gmail.com>
2017-11-24 09:11:42 -0500

    Update dog.py

--- plugins/dog.py
+++ plugins/dog.py
@@ -10,7 +10,7 @@ class Dog(Command):
 		self.client = pearl.client
 
 	def handle(self, args, event):
-		dog = self.client.upload_image("https://upload.wikimedia.org/wikipedia/commons/thumb/5/58/Shiba_inu_taiki.jpg/220px-Shiba_inu_taiki.jpg")
+		dog = self.client.upload_image("pearl/plugins/220px-Shiba_inu_taiki.jpg")
 		
 		asyncio.run_coroutine_threadsafe(self.send(dog, event.conversation_id.id), self.pearl.loop)
 
