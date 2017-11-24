7874bc9a140d6a70e9f74fdfb9dc620184b97283
Shwetha K <skunnamalt@gmail.com>
2017-11-23 21:54:04 -0500

    Create 8ball.py

---
+++ plugins/8ball.py
@@ -0,0 +1,4 @@
+import random
+responses = ["It is certain", "It is decidedly so", "Without a doubt", "Yes, definitely", "You may rely on it", "Reply hazy, try again", "Ask again later", "Better not tell you now", "Cannot predict now", "Error: 404 not found", "Don't count on it", "My reply is no", "My sources say no", "Outlook not so good", "My sources say no"]
+s = random.choice(responses)
+print(s)
